e5547b70679581721c94f049f3340f2a40a53515
tiredbluewhale <tired.blue.whale@gmail.com>
2020-11-20 16:48:56 +0200

    CHANGED hrefs

--- pages/info.js
+++ pages/info.js
@@ -9,9 +9,9 @@ export const Info = {
             <h1>Welcome to Kukka & Luca Cooking</h1>
             <p> This is a personal little receipt collection, feel free to use it :)</p>
             <div class="info_button_wrapper">
-                <a href="/#/appetizer">Appetizer</a>
-                <a href="/#/meals">Meals</a>
-                <a href="/#/deserts">Deserts</a>
+                <a href="./?appetizer">Appetizer</a>
+                <a href="./?meals">Meals</a>
+                <a href="./?deserts">Deserts</a>
             </div>
             </div>
         </div>
